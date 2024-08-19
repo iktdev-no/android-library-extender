a70747f12298e30696407088fcafee03e4787987
bskjon <bskjon@outlook.com>
2024-08-20 00:55:09 +0200

    Upgraded java and kotlin version

--- library/extender/src/main/java/no/iktdev/extender/LiveData.kt
+++ library/extender/src/main/java/no/iktdev/extender/LiveData.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 
 fun <T> LiveData<T>.observeOnceAfterInitUntilNotNull(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
-    observe(lifecycleOwner, object: Observer<T> {
+    observe(lifecycleOwner, object: Observer<T?> {
         var firstLoad = true
-        override fun onChanged(t: T?) {
-            if (t != null) {
-                observer.onChanged(t)
+        override fun onChanged(value: T?) {
+            if (value != null) {
+                observer.onChanged(value)
             }
             if (!firstLoad) {
                 removeObserver(this)
